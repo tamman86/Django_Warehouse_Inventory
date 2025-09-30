8910da3d051d0bded43644bd72520442c1d056c6
Allen <Allen.tam86@gmail.com>
2025-09-29 23:51:36 -0400

    Add root URL redirect

--- inventory_project/urls.py
+++ inventory_project/urls.py
@@ -4,11 +4,12 @@ from django.contrib.auth import views as auth_views
 from inventory import views as inventory_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/inventory/', permanent=True)),
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
-
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', inventory_views.logout_view, name='logout'),
 ]
